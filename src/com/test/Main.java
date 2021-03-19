4c2777b50b54f4eb5656aaee7755926086e65414
hasanshaih <hafizov.marat@mail.ru>
2021-03-19 22:16:57 +0300

    Initial commit

---
+++ src/com/test/Main.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+    }
+}
